6e766b87e6701d97e0660ff020536921c40a6cba
hu-lectures-abdo <149953538+hu-lectures-abdo@users.noreply.github.com>
2024-07-18 21:33:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// nav bar btn 
+let btn_nav = document.getElementById('btn');
+let nav_bar = document.querySelector('nav');
+btn_nav.addEventListener('click' , function(){
+    if(nav_bar.classList.contains('cancello')){
+        nav_bar.classList.remove('cancello');
+        nav_bar.classList.add('active');
+    }else {
+        nav_bar.classList.remove('active');
+        nav_bar.classList.add('cancello');
+    }
+})
+// 
+let a_mode = document.querySelector('.mode');
+let switch_btn = document.getElementById('switch-round');
+a_mode.addEventListener('click', function(){
+    if(switch_btn.getAttribute('checked') == null){
+        switch_btn.setAttribute('checked' , 'true')
+    }else {
+    switch_btn.removeAttribute('checked');
+
+    }
+    
+})
+let icon_mode = document.getElementById('icon_mode')
+let text_mode = document.getElementById('text_mode')
+a_mode.addEventListener('click', function(){
+    if(switch_btn.getAttribute('checked') == 'true'){
+        icon_mode.innerHTML = `<ion-icon name="sunny-outline"></ion-icon>`
+        text_mode.innerHTML = `light mode`
+        document.body.style.background = '#000';
+    }else {
+        icon_mode.innerHTML = `<ion-icon name="moon-outline"></ion-icon>`
+        text_mode.innerHTML = `dark mode`
+        document.body.style.background = '#daeaf7';
+
+    }
+}  )
