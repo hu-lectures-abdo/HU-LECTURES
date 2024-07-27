18ade47539855604c717e23ecb0e0f32c0ccee02
hu-lectures-abdo <149953538+hu-lectures-abdo@users.noreply.github.com>
2024-07-27 21:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,11 +62,11 @@ a_mode.addEventListener("click", function () {
 //
 let search = document.getElementById("search");
 let boxResult = document.querySelector(".box-result");
-let item = document.querySelectorAll(".container a h1 span");
+let item = document.querySelectorAll(".info");
 // to make unique id to elements
 for (let i = 0; i < item.length; i++) {
   item[i].setAttribute("data_id", Date.now() + i);
-  item[i].parentElement.parentElement.setAttribute("id", Date.now() + i + 1);
+  item[i].parentElement.setAttribute("id", Date.now() + i);
 }
 //
 let array = [];
@@ -85,13 +85,21 @@ function check() {
       search.value.length !== 0 &&
       search.value !== " "
     ) {
-      element_colne.innerHTML = `<a href='#${item[
-        i
-      ].parentElement.parentElement.getAttribute("id")}'>${
-        item[i].parentElement.children[0].innerHTML
-      }</a><br>`;
+      element_colne.innerHTML = `<a href='#${item[i].parentElement.getAttribute(
+        "id"
+      )}'>${item[i].innerHTML}</a><br>`;
       boxResult.appendChild(element_colne);
       array.push(element_colne);
+      element_colne.addEventListener("click", a);
+      function a() {
+        item.forEach((e) => {
+          e.parentElement.style.backgroundColor = "#ffffff80";
+        });
+        item[i].parentElement.style.backgroundColor = "#a1c1f4";
+        item[i].parentElement.onclick = function () {
+          item[i].parentElement.style.backgroundColor = "#ffffff80";
+        };
+      }
     }
   }
 }
@@ -126,8 +134,14 @@ function dark_mode() {
   let title = document.querySelector(".title p");
   let title_span = document.querySelector(".title p span");
   let box_text = document.querySelector(".pl");
-  // let sec_box = document.querySelectorAll('.glass');
-  let x_sign = document.querySelectorAll('x-sign');
+  let x_sign = document.querySelectorAll("x-sign");
+  let lectures = document.querySelectorAll(".l-b");
+  lectures.forEach((e) => {
+    e.addEventListener("click", function () {
+      let box_lect = document.querySelector(".box-lect");
+      box_lect.style.cssText = ` background-color :#292626; color:#fff !important; box-shadow: inset -4px -4px 5px rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4) 10px 6px 39px;`;
+    });
+  });
 
   //
 
@@ -152,37 +166,11 @@ function dark_mode() {
     "color:#ddd;border-top: 4px double #ddd;border-bottom: 4px double #ddd;";
   title_span.style.cssText = "-webkit-text-fill-color: transparent;";
   box_text.style.display = "none";
-  // sec_box.forEach((e)=> {
-  //   e.style.boxShadow = ` inset 4px 4px 5px rgba(255, 255, 255, 0.3), 
-  //                         inset -4px -4px 5px rgba(0, 0, 0, 0.1),
-  //                        rgba(0, 0, 0, 0.4) 10px 6px 39px `
-  // })
-  x_sign.forEach((e)=> {
-    e.style.cssText = `  color: yellow;
-            --color1: goldenrod;
-            --color2: orangered;
-            --color3: mediumblue;
-            --color4: purple;
-            font-family: Gruppo;
-            --interval: 1s;
-            display: block;
-            text-shadow: 
-              0 0 10px var(--color1),
-              0 0 20px var(--color2),
-              0 0 40px var(--color3),
-              0 0 80px var(--color4);
-            will-change: filter, color;
-            filter: saturate(60%);
-            font-size: 40px;
-            text-transform: uppercase;
-            white-space: nowrap;
-            font-weight: 400;
-            animation: flicker steps(100) var(--interval) 1s infinite;`
-  })
+  x_sign.forEach((e) => {
+    e.style.cssText = `  color: yellow; --color1: goldenrod; --color2: orangered; --color3: mediumblue; --color4: purple; font-family: Gruppo; --interval: 1s; display: block; text-shadow:    0 0 10px var(--color1),   0 0 20px var(--color2),   0 0 40px var(--color3),   0 0 80px var(--color4); will-change: filter, color; filter: saturate(60%);  text-transform: uppercase; white-space: nowrap; font-weight: 400; animation: flicker steps(100) var(--interval) 1s infinite;`;
+  });
 }
-
 //
-
 function light_mode() {
   let nav = document.querySelector("nav");
   let logo_h = document.querySelectorAll(".logo a");
@@ -197,8 +185,7 @@ function light_mode() {
   let title = document.querySelector(".title p");
   let title_span = document.querySelector(".title p span");
   let box_text = document.querySelector(".pl");
-  // let sec_box = document.querySelectorAll('.glass');
-  let x_sign = document.querySelectorAll('x-sign');
+  let x_sign = document.querySelectorAll("x-sign");
 
   //
 
@@ -224,37 +211,24 @@ function light_mode() {
     "color:#000;border-top: 4px double #000;border-bottom: 4px double #000;";
   title_span.style.cssText = "-webkit-text-fill-color: #000;";
   box_text.style.display = "block";
-  // sec_box.forEach((e)=> {
-  //   e.style.boxShadow = `-10px -10px 10px rgba(255, 255, 255, 0.2),
-  //                          15px 15px 15px rgba(0, 0, 0, .1),
-  //                         inset  -10px -10px 10px rgba(255, 255, 255, 0.2),
-  //                         inset  15px 15px 15px rgba(0, 0, 0, .1)`;
-  // })
-  x_sign.forEach((e)=> {
-    e.style.cssText = `  color: black;
-            font-family: Gruppo;
-            display: block;
-            text-shadow: 
-              0 0 10px var(--color1),
-              0 0 20px var(--color2),
-              0 0 40px var(--color3),
-              0 0 80px var(--color4);
-            will-change: filter, color;
-            filter: saturate(60%);
-            font-size: 40px;
-            text-transform: uppercase;
-            white-space: nowrap;
-            font-weight: 400;
-            animation: flicker steps(100) var(--interval) 1s infinite;`
-  })
+  x_sign.forEach((e) => {
+    e.style.cssText = `  color: black; font-family: Gruppo; display: block; text-shadow:    0 0 10px var(--color1),   0 0 20px var(--color2),   0 0 40px var(--color3),   0 0 80px var(--color4); will-change: filter, color; filter: saturate(60%);  text-transform: uppercase; white-space: nowrap; font-weight: 400; animation: flicker steps(100) var(--interval) 1s infinite;`;
+  });
+  let lectures = document.querySelectorAll(".l-b");
+  lectures.forEach((e) => {
+    e.addEventListener("click", function () {
+      let box_lect = document.querySelector(".box-lect");
+      box_lect.style.cssText = `   color:#000;background-color: #ffff;box-shadow: inset 4px 4px 5px rgba(255, 255, 255, 0.3), inset -4px -4px 5px rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.4) 10px 6px 39px;`;
+    });
+  });
 }
 //
 
 let counter = setInterval(() => {
   let date = document.querySelector("footer span");
-  let d = moment(new Date()).format("L");
-  let t = moment(new Date()).format("LT");
-  return (date.innerHTML = `${d} ${t}`);
+  // let d = moment(new Date()).format("L");
+  // let t = moment(new Date()).format("LT");
+  // return (date.innerHTML = `${d} ${t}`);
 }, 1000);
 setTimeout((_) => {
   ityped.init(document.querySelector("#ityed-mes"), {
@@ -262,30 +236,14 @@ setTimeout((_) => {
     loop: false,
   });
 }, 2000);
-let to = document.querySelector('.to-top')
-document.body.onscroll = function(){
-  if(scrollY >= 1000){
-    to.style.display = 'block';
-  }else {
-    to.style.display = 'none';
+let to = document.querySelector(".to-top");
+document.body.onscroll = function () {
+  if (scrollY >= 1000) {
+    to.style.display = "block";
+  } else {
+    to.style.display = "none";
   }
-}
-to.onclick = function() {
-window.scrollTo(0,0)
-}
-const signs = document.querySelectorAll('x-sign')
-const randomIn = (min, max) => (
-  Math.floor(Math.random() * (max - min + 1) + min)
-)
-
-const mixupInterval = el => {
-  const ms = randomIn(2000, 4000)
-  el.style.setProperty('--interval', `${ms}ms`)
-}
-
-signs.forEach(el => {
-  mixupInterval(el)
-  el.addEventListener('webkitAnimationIteration', () => {
-    mixupInterval(el)
-  })
-})
+};
+to.onclick = function () {
+  window.scrollTo(0, 0);
+};
